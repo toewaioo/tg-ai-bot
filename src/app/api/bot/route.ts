9a89cd31a8f156e5c004287b4f1b0eb686362942
DelNyal <delnyal220@gmail.com>
2025-10-30 01:57:05 +0000

    error fix#3

---
+++ src/app/api/bot/route.ts
@@ -0,0 +1,21 @@
+export const dynamic = 'force-dynamic'
+
+export const fetchCache = 'force-no-store'
+
+import { NextRequest, NextResponse } from 'next/server';
+
+import { Bot, webhookCallback } from 'grammy'
+import { bot } from '@/lib/bot-instance';
+import { setupCommands } from '@/lib/bot-commands';
+setupCommands(bot);
+bot.on('message:text', async (ctx) => {
+  await ctx.reply(ctx.message.text)
+})
+
+export const POST = webhookCallback(bot, 'std/http')
+export async function GET() {
+  return NextResponse.json({
+    ok: true,
+    message: '✅ Telegram webhook endpoint is live.',
+  });
+}
