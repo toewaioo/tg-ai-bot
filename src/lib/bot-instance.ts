daa18ac52bbb124b27bd339e2a86a7cacac4b62e
DelNyal <delnyal220@gmail.com>
2025-11-08 09:54:22 +0000

    this this erro for cron route 404 This page could not be found.

--- src/lib/bot-instance.ts
+++ src/lib/bot-instance.ts
@@ -1,21 +1,17 @@
 import { Bot } from 'grammy';
 import { setupCommands } from './bot-commands';
-import { setupForwardVideosCommands } from './forward-command';
 
 // Use NEXT_PUBLIC_TELEGRAM_BOT_TOKEN to be consistent and available for the client-side link.
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 if (!token) {
   console.error(
-    'NEXT_PUBLIC_TELEGRAM_BOT_TOKEN is not set! The bot will not be able to start.'
+    'TELEGRAM_BOT_TOKEN is not set! The bot will not be able to start.'
   );
 }
 
 // Create bot object
 export const bot = new Bot(token || '');
 
-//
-setupForwardVideosCommands(bot)
 // Register commands
 setupCommands(bot);
-
